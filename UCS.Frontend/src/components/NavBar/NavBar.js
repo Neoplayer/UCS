954b263e569127e2d0990ab99f08dbf8efd7659d
amandany <dan.aman@yandex.ru>
2022-04-11 22:43:30 +0300

    username fix

--- UCS.Frontend/src/components/NavBar/NavBar.js
+++ UCS.Frontend/src/components/NavBar/NavBar.js
@@ -38,7 +38,7 @@ const NavBar = () => {
         </Link>
         <br />
 
-        <h1 className="person">Аман Даниил Владимирович</h1>
+        <h1 className="person">{User.User && User.User.username }</h1>
         <div className="avatar-wrapper">
           <img
             src={`${process.env.PUBLIC_URL}/peeps/peep-${Peeps}.png`}
