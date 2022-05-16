feb761a74f21a4cfe8931557b5eff27ae5260ae3
NeerdOner <pas25151@gmail.com>
2022-05-16 22:26:08 +0300

    oh

--- UCS.WebApi/Dto/Session/SessionResponse.cs
+++ UCS.WebApi/Dto/Session/SessionResponse.cs
@@ -5,6 +5,7 @@ namespace UCS.WebApi.Dto.Session
         public TopicResponse TopicInfo { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime TimeLimit { get; set; }
+        public TimeSpan TimeLeft { get; set; }
 
         public List<QuestionResponse> Questions { get; set; }
     }
